f82c947df16c045c5da9649559b0a561c3e32a50
dark-tracer <bernieamponsah2@gmail.com>
2024-04-14 04:24:33 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Module that deletes states containing the letter\
+"""Module that deletes states containing the letter
         "a" from a MySQL database using SQLAlchemy."""
 import sys
 from sqlalchemy import create_engine
