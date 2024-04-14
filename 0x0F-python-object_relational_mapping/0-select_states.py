b6b4d4a9e877f6a18dbe49e029a099e7dfc2ff9e
dark-tracer <bernieamponsah2@gmail.com>
2024-04-14 03:36:34 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -5,31 +5,14 @@
 import sys
 import MySQLdb
 
-def list_states (username, password, database):
-    
-    # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
-    cursor = db.cursor()
-
-    # Execute the SQL query to fetch all states
-    cursor.execute("SELECT * FROM states ORDER BY id ASC")
-
-    # Fetch all the rows from the query result
-    rows = cursor.fetchall()
-
-    # Print the results
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+            passwd=sys.argv[2], d =sys.argv[3], port=3306)
+    c = db.cursor()
+    c.execute("SELECT * FROM states")
+    rows = c.fetchall()
     for row in rows:
         print(row)
-
-    # Close the database connection
+    c.close()
     db.close()
-
-# Example usage
-if __name__ == '__main__':
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
-
-    list_states(username, password, database)
 
