c7354f8e2f4be5b18ae95b66a9441d2d0dc79b7e
dark-tracer <bernieamponsah2@gmail.com>
2024-04-14 03:20:41 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,18 +6,9 @@ import sys
 import MySQLdb
 
 def list_states (username, password, database):
-    """lists all states from the database hbtn_0e_0_usa.
-    Ags:
-        username: mysql username
-        password: mysql password
-        database: mysql database
-    """
+    
     # Connect to the MySQL server
-    db = MySQLdb.connect(host='localhost',\
-            port=3306,\
-            user=username,\
-            passwd=password,\
-            db=database)
+    db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=database)
     cursor = db.cursor()
 
     # Execute the SQL query to fetch all states
