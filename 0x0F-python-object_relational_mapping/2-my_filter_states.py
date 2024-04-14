8f0649ef3a9d4730cfc20284233c25e7f9ae4cc2
dark-tracer <bernieamponsah2@gmail.com>
2024-04-14 03:01:54 -0700

    0x0F. Python - Object-relational mapping

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,44 +1,22 @@
 #!/usr/bin/python3
+
 """Module that lists all states from the hbtn_0e_0_usa database."""
+
 import sys
 import MySQLdb
 
 if __name__ == "__main__":
-    import MySQLdb
-    import sys
 
-    # Get the command-line arguments
-    mysql_username = sys.argv[1]
-    mysql_password = sys.argv[2]
-    database_name = sys.argv[3]
-    state_name = sys.argv[4]
+    # Get MySQL credentials and search name from command-line arguments
+    # and # Connect to MySQL server
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
 
-    # Connect to the MySQL server
-    db = MySQLdb.connect(
-        host='localhost',
-        port=3306,
-        user=mysql_username,
-        passwd=mysql_password,
-        db=database_name
-    )
+    # Execute the SQL query to retrieve states with the specified name
+    c.execute("SELECT * \
+                 FROM `states` \
+                WHERE BINARY `name` = '{}'".format(sys.argv[4]))
 
-    # Create a cursor object to execute queries
-    cursor = db.cursor()
-
-    # Prepare the SQL query with placeholders
-    sql_query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-
-    # Execute the query with the state name as a parameter
-    cursor.execute(sql_query, (state_name,))
-
-    # Fetch all the rows returned by the query
-    rows = cursor.fetchall()
-
-    # Display the results
-    for row in rows:
-        print(row)
-
-    # Close the cursor and database connection
-    cursor.close()
-    db.close()
+    # Fetch all rows and print the states
+    [print(state) for state in c.fetchall()]
 
